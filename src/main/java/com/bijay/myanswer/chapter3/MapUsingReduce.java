b1c2250c03b7d08803cd841808960133d01bcacc
bijaydeo <deo.developer@gmail.com>
2016-08-23 20:46:02 -0400

    more

--- src/main/java/com/bijay/myanswer/chapter3/MapUsingReduce.java
+++ src/main/java/com/bijay/myanswer/chapter3/MapUsingReduce.java
@@ -1,12 +1,21 @@
 package com.bijay.myanswer.chapter3;
 
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
 /**
  * Advanced Exercises Question 2
  */
 public class MapUsingReduce {
 
     public static <I, O> java.util.List<O> map(java.util.stream.Stream<I> stream, java.util.function.Function<I, O> mapper) {
-        return com.insightfullogic.java8.exercises.Exercises.replaceThisWithSolution();
+        /*List<O> output = new ArrayList<O>();
+        return  stream.reduce(output,
+                (oList,i)-> {oList.add(mapper.apply(i)); return oList;},
+                );
+                */
+        //TODO: yet to be implemented
+        throw new RuntimeException("not yet implemented");
+        SummaryStatistics
     }
 
 }
