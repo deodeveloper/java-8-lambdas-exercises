94ee1e5b7a3bdf7fb75c8f5cbb6fc2afe409500d
bijaydeo <deo.developer@gmail.com>
2017-02-25 18:57:44 -0500

    GroupingBy Implementation on chapter 5

--- src/main/java/com/bijay/myanswer/chapter5/GroupingBy.java
+++ src/main/java/com/bijay/myanswer/chapter5/GroupingBy.java
@@ -1,10 +1,6 @@
 package com.bijay.myanswer.chapter5;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -12,36 +8,43 @@ import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 public class GroupingBy<T, K> implements Collector<T, Map<K, List<T>>, Map<K, List<T>>> {
+  static Set<Characteristics> set = new HashSet<>();
+  static {
+    set.add(Characteristics.IDENTITY_FINISH);
+  }
+  private final Function<? super T, ? extends K> classifier;
 
-    private final Function<? super T, ? extends K> classifier;
+  public GroupingBy(Function<? super T, ? extends K> classifier) {
+    this.classifier = classifier;
+  }
 
-    public GroupingBy(Function<? super T, ? extends K> classifier) {
-        this.classifier = classifier;
-    }
+  @Override
+  public Supplier<Map<K, List<T>>> supplier() {
+    return HashMap::new;
+  }
 
-    @Override
-    public Supplier<Map<K, List<T>>> supplier() {
-        return Exercises.replaceThisWithSolution();
-    }
+  @Override
+  public BiConsumer<Map<K, List<T>>, T> accumulator() {
+    return (map, s) -> {
+      List val = map.computeIfAbsent(classifier.apply(s), k->new ArrayList<>());
+      val.add(s);
+    };
+  }
 
-    @Override
-    public BiConsumer<Map<K, List<T>>, T> accumulator() {
-        return Exercises.replaceThisWithSolution();
-    }
+  @Override
+  public BinaryOperator<Map<K, List<T>>> combiner() {
+    return (m1, m2) -> {throw new RuntimeException("No parallel execution allowed on GroupingBy. expected to run sequentially.");};
+  }
 
-    @Override
-    public BinaryOperator<Map<K, List<T>>> combiner() {
-        return Exercises.replaceThisWithSolution();
-    }
+  @Override
+  public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
+    return m->m;
+  }
 
-    @Override
-    public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
-        return Exercises.replaceThisWithSolution();
-    }
-
-    @Override
-    public Set<Characteristics> characteristics() {
-        return Exercises.replaceThisWithSolution();
-    }
+  @Override
+  public Set<Characteristics> characteristics() {
+    return set;
+  }
 
 }
+
